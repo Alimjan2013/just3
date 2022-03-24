2b8fbaad4c20f446fe6ff3aa2947457ea621459c
Alimjan <alimjan@zju.edu.cn>
2022-03-24 19:32:11 +0800

    init react app

---
+++ src/components/things.js
@@ -0,0 +1,19 @@
+
+const Thing = ()=>{
+
+    return (
+       <div>
+            thing1
+       </div> 
+    )
+}
+const Things = ()=>{
+    return(
+        <div>
+         <Thing/>
+        </div>  
+    )
+}
+
+
+export default Things
