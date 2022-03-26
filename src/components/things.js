30e0342d4a2c2955062c35ae7d3193ae6370af79
Alimjan <alimjan@zju.edu.cn>
2022-03-26 12:20:20 +0800

    更新数据到数据库

--- src/components/things.js
+++ src/components/things.js
@@ -1,16 +1,22 @@
+import React ,{useState,useEffect} from 'react'
 
 const Thing = (props)=>{
     let thingName = props.thing
-    let status = props.things[thingName]
-    const handleOnClick = ()=>{
+    const [status,setState] =useState()
 
-        status = !status
+    useEffect(() => {
+        setState(props.things[thingName])
+      }, []);
+
+    const handleOnClick = ()=>{
+        props.changestatus(thingName,!status)
+        setState(!status)
     }
     return (
        <div className="flex-1 flex flex-col items-center space-y-2 ">
         <input 
             onClick={()=>{handleOnClick()}}  
-            className="bg-fill-1 rounded-md bg-fill-2   w-20 h-20  border-4 border-black appearance-none checked:bg-green-1    " 
+            className=" rounded-md bg-fill-2   w-20 h-20  border-4 border-black appearance-none checked:bg-green-1    " 
             type="checkbox" 
             defaultChecked = {status}
             />
@@ -26,12 +32,25 @@ const Things = (props)=>{
     const ThingList = props.things.things
     let thingsGroup
     console.log(ThingList)
-    
+    const changestatus =(key,status)=>{
+        console.log(status)
+        console.log()
+        fetch('https://qcuud7.api.cloudendpoint.cn/updateThing',{
+            method:"POST",
+            headers: {'Content-Type': 'application/json'},
+            body:JSON.stringify({id: props.things._id,thingName:key,status:status})
+          })
+            .then(res=>res.json())
+            .then(
+              json=>{
+                console.log(json.test)
+              })  
+    }
 
     if(ThingList === undefined ){
         thingsGroup =''
     }else{
-        thingsGroup = Object.keys(ThingList).map((item) => <Thing key={item} things={props.things.things} thing={item}/>)
+        thingsGroup = Object.keys(ThingList).map((item) => <Thing key={item} things={props.things.things} thing={item} changestatus={changestatus.bind()}/>)
     }
     
     return(
