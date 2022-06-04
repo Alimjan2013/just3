fd9a2cd6ac91025507c252cd10b0eb41bb7bdc0b
Alimjan2013 <alimujiang1414@qq.com>
2022-06-04 12:46:17 +0800

    add func getThingsFromDB

--- src/dateRange.py
+++ src/dateRange.py
@@ -1,17 +1,27 @@
 from datetime import date
 from datetime import timedelta
+import json
+from tracemalloc import start
+import requests
+
+
+userID = 'Alimjan'
 
 
 def initDate(Onedate):
+    # TODO 需要判断输入的日期符不符合YYYY-MM-DD 的规范
     NewDate = date.fromisoformat(Onedate)
     return NewDate
 
 
 def getDateRange(Onedate, mode):
+    # 输入日期(yyyy-mm-dd)，返回此日期所在周的开始与结束
+    # 默认 周日为开始，周六为结束，
+    # TODO:后续改为 ISO 标准日历
+    day = initDate(Onedate)
+
     def getWeekRange(Onedate):
-        # 输入日期，返回此日期所在周的开始与结束
-        # 默认 周日为开始，周六为结束，
-        # TODO:后续改为 ISO 标准日历
+
         weekDay = date.isoweekday(Onedate)
         if(weekDay == 7):
             weekDay = 0
@@ -37,26 +47,45 @@ def getDateRange(Onedate, mode):
         return {'start': start, 'end': end}
 
     if(mode == 'week'):
-        Range = getWeekRange(Onedate)
+        Range = getWeekRange(day)
     elif (mode == 'month'):
-        Range = getMonthRange()
+        Range = getMonthRange(day)
     else:
         # 如使用了其他mode，返回-1 代表错误
         Range = -1
     return Range
 
 
-# 主函数入口
+def getThingsFromDB(range, userID):
+    start = date.isoformat(range['start'])
+    end = date.isoformat(range['end'])
+    url = 'https://rxvkdzbhzca45kuqjd3ein6sea0vhido.lambda-url.ap-east-1.on.aws/'
+    values = {
+        "userID": userID,
+        "type": "range",
+        "timeRange": {
+            "start": start,
+            "end": end
+        },
+    }
+
+    headers = {'Content-Type': 'application/json'}
+    thingsFromDB = requests.post(url=url, data=json.dumps(
+        values), headers=headers).json()
+
+    return thingsFromDB
+
+
 def DifferenceOfRange(Onedate, mode):
-    # 提供一个日期和模式（周还是月）
+    # 提供一个日期和mode（周还是月）
     # 返回所提供时长内每日的结果，如当日没有记录记为空
     range = getDateRange(Onedate, mode)
-    ThingsArrayFromDB = getThingsFromDB(range)
+    ThingsArrayFromDB = getThingsFromDB(range, userID)
     emptyRangeArray = creatDateArrayWithRange(range)
     DifferenceOfArray = CompareTwoArray(emptyRangeArray, ThingsArrayFromDB)
     return DifferenceOfArray
 
 
-initDate()
+# 主函数入口
 DifferenceOfRange()
 initArray()
